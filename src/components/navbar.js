6af3336f2091c2d5bdfff30a318f39cddee29a9d
Evan <evankurokawa@gmail.com>
2022-05-12 21:32:34 -0600

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,28 +7,24 @@ export default function NavBar() {
         <header className="bg-stone-100 ">
             <div className="'mx-auto', mapMaxWidthStyles(headerWidth), {
                     'xl:border-l xl:border-r border-current': headerWidth === 'narrow',
-                    '2xl:border-l 2xl:border-r border-current': headerWidth === 'wide w-full">
+                    '2xl:border-l 2xl:border-r border-current': headerWidth === 'wide w-full border-b border-current border-dashed">
                 <nav className="flex items-stretch relative">
                     <NavLink to={'/'} 
-                        className='inline-flex items-center px-3 mr-4 tracking-widest uppercase font-semibold border-r border-current'>
+                        className='inline-flex items-center px-3 mr-4 tracking-widest uppercase font-semibold border-r border-current transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 border-dashed'>
                             C.S.O Design Studio
                     </NavLink>
-                    <div  className="hidden lg:flex border-current ml-auto uppercase border-l">
+                    <div  className="hidden lg:flex border-current ml-auto uppercase border-l border-dashed">
                         <NavLink to={'/portfolio'} 
-                            className='inline-flex items-center py-2 px-3 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
+                            className='inline-flex items-center py-2 px-4 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
                                 Portfolio
                         </NavLink>
                         <NavLink to={'/about'}  
-                            className='inline-flex items-center py-2 px-3 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
+                            className='inline-flex items-center py-2 px-4 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
                                 About
                         </NavLink>
                         <NavLink to={'/contact'} 
-                            className='inline-flex items-center py-2 px-3 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
+                            className='inline-flex items-center py-2 px-4 tracking-widest transition ease-in delay-85 hover:scale-100 hover:bg-stone-50 duration-300 font-normal '>
                                 Contact
-                        </NavLink>
-                        <NavLink to={'/blog'} 
-                            className='inline-flex items-center py-2 px-3 tracking-widest transition ease-in delay-85 hover:scale-10 hover:bg-stone-50 duration-300 font-normal '>
-                                Blog
                         </NavLink>
                     </div>
                     
